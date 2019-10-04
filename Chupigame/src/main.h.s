d46b209850959effdf2a45c8e16e85dbbed0b40d
danieemil <danieemil@hotmail.com>
2019-10-04 17:42:06 +0200

    Cambiamos a lenguaje ensamblador

---
+++ Chupigame/src/main.h.s
@@ -0,0 +1,9 @@
+.globl _g_palette
+.globl _player_spr
+
+.globl cpct_disableFirmware_asm
+.globl cpct_getScreenPtr_asm
+.globl cpct_setVideoMode_asm
+.globl cpct_setPalette_asm
+.globl cpct_drawSprite_asm
+.globl cpct_setPALColour_asm
---
+++ Chupigame/src/main.s
@@ -0,0 +1,82 @@
+;;-----------------------------LICENSE NOTICE------------------------------------
+;;  This file is part of CPCtelera: An Amstrad CPC Game Engine 
+;;  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Lesser General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Lesser General Public License for more details.
+;;
+;;  You should have received a copy of the GNU Lesser General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;-------------------------------------------------------------------------------
+
+;; Include all CPCtelera constant definitions, macros and variables
+.include "cpctelera.h.s"
+.include "main.h.s"
+.include "entity.h.s"
+;;
+;; Start of _DATA area 
+;;  SDCC requires at least _DATA and _CODE areas to be declared, but you may use
+;;  any one of them for any purpose. Usually, compiler puts _DATA area contents
+;;  right after _CODE area contents.
+;;
+.area _DATA
+
+;;
+;; Start of _CODE area
+;; 
+.area _CODE
+
+;; 
+;; Declare all function entry points as global symbols for the compiler.
+;; (The linker will know what to do with them)
+;; WARNING: Every global symbol declared will be linked, so DO NOT declare 
+;; symbols for functions you do not use.
+;;
+
+
+
+;;
+;; MAIN function. This is the entry point of the application.
+;;    _main:: global symbol is required for correctly compiling and linking
+;;
+_main::
+   ;; Disable firmware to prevent it from interfering with string drawing
+   call cpct_disableFirmware_asm
+
+   ld c, #1
+   call cpct_setVideoMode_asm          ;;Destruye AF, BC, HL
+
+   ld hl, #_g_palette
+   ld de, #4
+   call cpct_setPalette_asm            ;;Destruye AF, BC, DE, HL
+
+
+   ld hl, #0x0B10
+   call cpct_setPALColour_asm          ;;Destruye F, BC, HL
+
+
+   ;; Calculate a video-memory location for printing a string
+   ld   de, #CPCT_VMEM_START_ASM ;; DE = Pointer to start of the screen
+   ;;ld    b, #24                  ;; B = y coordinate (24 = 0x18)
+   ;;ld    c, #16                  ;; C = x coordinate (16 = 0x10)
+   ld   bc, #0x1810
+
+   call cpct_getScreenPtr_asm    ;; Calculate video memory location and return it in HL
+
+   ex de, hl
+   ld hl, #_player_spr
+   ld bc, #0x1004
+   call cpct_drawSprite_asm            ;;Destruye AF, BC, DE, HL
+
+
+
+   ;; Loop forever
+loop:
+   jr    loop
